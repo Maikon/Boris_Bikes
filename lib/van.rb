1e3d68fd5852c46bbffdabc2f0450ed75fa6ecc6
Makis Otman <makisotman@gmail.com>
2014-01-22 17:16:37 +0000

    removed unnecessary code from van.rb

--- lib/van.rb
+++ lib/van.rb
@@ -3,12 +3,4 @@ require_relative 'bike_container'
 class Van
   include BikeContainer
 
-  def initialize(options = {})
-    self.capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
-  end
-
-  def van
-    @van ||= []
-  end
-
 end
