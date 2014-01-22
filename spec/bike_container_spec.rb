78b3700fac7aa07ef9945aea050cc510cb020c79
Makis Otman <makisotman@gmail.com>
2014-01-22 12:01:17 +0000

    reduced docking_station_spec to one test checking for the ability to set default capacity upon initialisation

--- spec/bike_container_spec.rb
+++ spec/bike_container_spec.rb
@@ -12,6 +12,10 @@ describe BikeContainer do
   let(:bike) { Bike.new }
   let(:holder) { ContainerHolder.new(capacity: 20) }
 
+  def fill_station(station)
+    20.times { station.dock(bike) }
+  end
+
   it 'should be able to accept a bike' do
     expect(holder.bike_count).to eq(0)
     holder.dock(bike)
@@ -26,12 +30,12 @@ describe BikeContainer do
   end
 
   it 'should know when it\'s full' do
-    20.times { holder.dock(bike) }
+    fill_station(holder)
     expect(holder.full?).to be_true
   end
 
   it 'should not accept any bikes if it\'s full' do
-    20.times { holder.dock(bike) }
+    fill_station(holder)
     expect { holder.dock(bike) }.to raise_error(RuntimeError)
   end
 end
